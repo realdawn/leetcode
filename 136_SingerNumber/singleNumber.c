1416f95c575223616352dd3e1e32f069513c0b0d
lidong <liong_li@foxmail.com>
2016-01-25 22:12:46 +0800

    Add 136. It accepted.

--- 136_SingerNumber/singleNumber.c
+++ 136_SingerNumber/singleNumber.c
@@ -1,3 +1,14 @@
 int singleNumber(int* nums, int numsSize) {
-
+	/* a XOR b xor a = b
+ 	 * a XOR b = (a&~b)|(~a&b)
+	 * ~b = -(b+1)	
+	 */
+	while (numsSize > 1)
+	{
+		*(nums + 1) = (*nums & -(*(nums + 1) + 1))
+			      | (-(*nums + 1) & *(nums + 1));
+		nums++;
+		numsSize--;
+	}
+	return *nums;
 }
